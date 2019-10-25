93873116739ff79e999627d25888c9587a35fc40
herick achu <herickoachu@gmail.com>
2019-10-25 22:58:54 +0200

    update

--- project.java
+++ project.java
@@ -32,7 +32,74 @@ Cart class
 TEST
 Application class
 VARIABLES AND METHODS OF THE VARIOUS CLASSES
-1 Person Class.
+1 Person Class
+name 
+age
+address
+emailAddress
+phoneNumber
+gender
+
+EMPLOYEE CLASS
+VARIABLES
+
+staffNumber
+position
+password
+userName
+idnumber
+
+
+METHODS
+
+sellproducts
+registerProducts(manager)
+voidProducts(manager)
+login
+
+CUSTOMER
+
+card
+
+METHODS
+
+sellproducts
+buyProducts
+
+Relationship between classes above
+1 customer is a person
+2 employee is a person
+
+COMPANY CLASS
+
+name
+address
+emailAddress
+phoneNumber
+
+METHODS
+
+employeeregistration
+login
+printreceipt
+
+Relationship
+
+company has employees
+
+
+invetory has a relationship with supplier
+
+SUPPLIER
+
+supplier supply products
+supplier is a company
+
+
+
+
+
+
 
  
 
